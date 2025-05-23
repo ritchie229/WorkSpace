4227d92dd4860633ffa0e2ca43aa7f6a91566b4d
Rishat Mukhtarov <158841983+ritchie229@users.noreply.github.com>
2025-05-24 02:25:16 +0300

    Final 1

--- Docker_Learn/tomcat/script.sh
+++ Docker_Learn/tomcat/script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# first run ending commands like start a service, copy a file etc, at the end put a command starting a program
 service ssh start && sleep 10 && /usr/sbin/apache2ctl -DFOREGROUND
