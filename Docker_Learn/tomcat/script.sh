6d0fe97741189f795689d28d86e19e4bc0cddf47
Rishat Mukhtarov <158841983+ritchie229@users.noreply.github.com>
2025-05-25 16:51:12 +0300

    Env pre Final 1

--- Docker_Learn/tomcat/script.sh
+++ Docker_Learn/tomcat/script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # first run ending commands like start a service, copy a file etc, at the end put a command starting a program
+sed -i 's|%CONTAINER_NAME%|'"$CONTAINER_NAME"'|g' /var/www/html/index.html
+sed -i 's|%container_hostname%|'"$HOSTNAME"'|g' /var/www/html/index.html 
+sed -i 's|%ipv4_address%|'"$ipv4_address"'|g' /var/www/html/index.html
+
 service ssh start && sleep 10 && /usr/sbin/apache2ctl -DFOREGROUND
