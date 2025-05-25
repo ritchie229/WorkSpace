0842cdf8d4c559938e9c93a962cc205c49fce2c0
Rishat Mukhtarov <158841983+ritchie229@users.noreply.github.com>
2025-05-25 17:14:37 +0300

    Final env 02

--- Docker_Learn/tomcat/script.sh
+++ Docker_Learn/tomcat/script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # first run ending commands like start a service, copy a file etc, at the end put a command starting a program
+export ipv4_address=$(echo | hostname -i)
+export OS=$(cat /etc/os-release | grep PRETTY_NAME | cut -d "=" -f)
 sed -i 's|%CONTAINER_NAME%|'"$CONTAINER_NAME"'|g' /var/www/html/index.html
 sed -i 's|%container_hostname%|'"$HOSTNAME"'|g' /var/www/html/index.html 
 sed -i 's|%ipv4_address%|'"$ipv4_address"'|g' /var/www/html/index.html
-
+sed -i 's|%OS%|'"$OS"'|g' /var/www/html/index.html
+sed -i 's|%main_owner%|'"$OWNER"'|g' /var/www/html/index.html
 service ssh start && sleep 10 && /usr/sbin/apache2ctl -DFOREGROUND
